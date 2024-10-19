3fa784c36f83eeec3bdeebe70ecbbbc5c02e6f61
k1mqq <keemacurry238@gmail.com>
2024-10-19 15:57:58 +0900

    basePath削除

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import Image from "next/image";
-import { basePath } from "../next.config.mjs" // 追加
-const BASE_PATH = basePath ? basePath : "" // 追加
 
 
 export default function Home() {
   
   return (
-    <Image src={`${BASE_PATH}/myicon.png`} width={100} height={100} alt={"this is keemacurry"}></Image>
+    <Image src={`/myicon.png`} width={100} height={100} alt={"this is keemacurry"}></Image>
   );
 }
