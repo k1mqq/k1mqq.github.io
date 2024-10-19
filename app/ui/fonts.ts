eb3231bdede34d8821448a881b30249a7381e6c5
k1mqq <keemacurry238@gmail.com>
2024-10-19 16:34:10 +0900

    fonts追加

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Zen_Kaku_Gothic_New } from 'next/font/google';
+ 
+export const zen_kaku_gothic_new = Zen_Kaku_Gothic_New({
+    subsets:['latin-ext','cyrillic','latin'],
+    weight:['400']
+});
