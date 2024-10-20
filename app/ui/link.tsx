b577c5d6ddc794f6ef336be74d7cbe3310ba4d97
k1mqq <keemacurry238@gmail.com>
2024-10-20 11:18:56 +0900

    Revert "panel共通化"
    This reverts commit 0b8b43180723463ffa93b63500b469d9ba58a443.

--- app/ui/link.tsx
+++ app/ui/link.tsx
@@ -1,32 +1,15 @@
-import Panel from "@/app/ui/panel";
 export default function Link() {
-  return (
-    <div className="flex items-center justify-center">
-      <Panel>
-        <a
-          href="https://x.com/k1mq_"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="m-3"
-        >
-          <p className="mx-auto text-center border-b-2 text-xl transition ease-in hover:border-cyan-500">
-            X(Twitter)
-          </p>
-        </a>
-        <a
-          href="https://github.com/k1mqq"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="m-3"
-        >
-          <p className="mx-auto text-center border-b-2 text-xl transition ease-in hover:border-cyan-500">
-            Github
-          </p>
-        </a>
-        <p className="mx-auto text-right text-sm">
-          まとめるほどたくさんリンクなかった
-        </p>
-      </Panel>
-    </div>
-  );
+    return (
+        <div className="flex items-center justify-center">
+          <div className="w-full md:w-2/4 m-5 p-4 bg-gray-100/10 rounded-lg border backdrop-blur-lg shadow-lg border-gray-100/50 text-slate-100">
+            <a href="https://x.com/k1mq_" target="_blank" rel="noopener noreferrer" className="m-3">
+              <p className="mx-auto text-center border-b-2 text-xl transition ease-in hover:border-cyan-500">X(Twitter)</p>
+            </a>
+            <a href="https://github.com/k1mqq" target="_blank" rel="noopener noreferrer" className="m-3">
+              <p className="mx-auto text-center border-b-2 text-xl transition ease-in hover:border-cyan-500">Github</p>
+            </a>
+            <p className="mx-auto text-right text-sm">まとめるほどたくさんリンクなかった</p>
+          </div>
+        </div>
+      )
 }
