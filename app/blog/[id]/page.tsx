4a3e08138db86888c2bba19c4127360e669243c9
k1mqq <keemacurry238@gmail.com>
2024-10-20 23:30:18 +0900

    ブログ内容調整

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,21 +1,30 @@
-import { getMarkdownFiles, getMarkdown, markdownToHtml } from "@/app/lib/markdown-util"
+import {
+  getMarkdownFiles,
+  getMarkdown,
+  markdownToHtml,
+} from "@/app/lib/markdown-util";
 
 export function generateStaticParams() {
-    const posts = getMarkdownFiles("blog-posts");
-  
-    return posts.map((post) => ({
-        id: post,
-    }));
-  }
+  const posts = getMarkdownFiles("blog-posts");
 
-  interface PostProps {
-    id: string
-  }
+  return posts.map((post) => ({
+    id: post,
+  }));
+}
 
-  export default async function Page({ params }:{ params: PostProps }) {
-    const htmlContent = await markdownToHtml(getMarkdown(`blog-posts/${params.id}.md`).content);
-    return (
-      <div dangerouslySetInnerHTML={{ __html:htmlContent}}>
+interface PostProps {
+  id: string;
+}
+
+export default async function Page({ params }: { params: PostProps }) {
+  const htmlContent = await markdownToHtml(
+    getMarkdown(`blog-posts/${params.id}.md`).content
+  );
+  return (
+    <div className="flex items-center justify-center">
+      <div className="w-full md:w-2/4 m-5 p-4 bg-gray-100/10 rounded-lg backdrop-blur-lg shadow-lg border-gray-100/30 text-slate-100">
+        <div dangerouslySetInnerHTML={{ __html: htmlContent }}></div>
       </div>
-    );
-  }
+    </div>
+  );
+}
