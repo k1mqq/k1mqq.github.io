07e6e1312d385c1bafe12938213b107ddb752aaa
k1mqq <keemacurry238@gmail.com>
2024-10-20 23:18:59 +0900

    ブログpage調整

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -5,20 +5,22 @@ import { getMarkdownFiles } from "@/app/lib/markdown-util";
 // import type { GetStaticProps, NextPage } from "next";
 
 export default function Home() {
-    const markdownFiles = getMarkdownFiles("blog-posts");
+  const markdownFiles = getMarkdownFiles("blog-posts");
 
-    return (
-    <div className="container">
-      <h1>記事一覧</h1>
-      <ul>
-        {markdownFiles.map((slug, i) => {
-          return (
-            <li key={i}>
-              <Link href={`/blog/${slug}`}>{slug}</Link>
-            </li>
-          );
-        })}
-      </ul>
+  return (
+    <div className="flex items-center justify-center">
+      <div className="w-full md:w-2/4 m-5 p-4 bg-gray-100/10 rounded-lg border backdrop-blur-lg shadow-lg border-gray-100/50 text-slate-100">
+        <h1>記事一覧</h1>
+        <ul>
+          {markdownFiles.map((slug, i) => {
+            return (
+              <div key={i} className="mx-auto my-2 p-4 bg-gray-100/10 rounded-lg border backdrop-blur-lg shadow-lg border-gray-100/50 text-slate-100">
+                <Link href={`/blog/${slug}`} ><div className="p-2 mx-auto border-b-2 hover:border-cyan-500/80 transition ease-in">{slug}</div></Link>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
