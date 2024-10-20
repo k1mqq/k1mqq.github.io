89c1bcb95073572f334b1b115757f9b0ed4ef3d5
k1mqq <keemacurry238@gmail.com>
2024-10-20 23:05:53 +0900

    ブログ機能

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { getMarkdownFiles } from "@/app/lib/markdown-util"
+import { getMarkdownFiles, getMarkdown, markdownToHtml } from "@/app/lib/markdown-util"
 
 export function generateStaticParams() {
     const posts = getMarkdownFiles("blog-posts");
@@ -12,10 +12,10 @@ export function generateStaticParams() {
     id: string
   }
 
-  export default function Page({ params }:{ params: PostProps }) {
+  export default async function Page({ params }:{ params: PostProps }) {
+    const htmlContent = await markdownToHtml(getMarkdown(`blog-posts/${params.id}.md`).content);
     return (
-      <div>
-        <p>{params.id}</p>
+      <div dangerouslySetInnerHTML={{ __html:htmlContent}}>
       </div>
     );
   }
